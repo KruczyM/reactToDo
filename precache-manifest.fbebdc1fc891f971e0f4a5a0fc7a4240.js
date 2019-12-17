49f5d25f582fb1d6c5580c41caa06ecb4c587a8c
KruczyM <kruczus5@gmail.com>
2019-12-17 09:18:05 +0100

    Updates

---
+++ precache-manifest.fbebdc1fc891f971e0f4a5a0fc7a4240.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7264aec59442705317e",
+    "url": "/reactToDo/static/js/main.a7264aec.chunk.js"
+  },
+  {
+    "revision": "1826e3765ed8d6a8f2df",
+    "url": "/reactToDo/static/js/1.1826e376.chunk.js"
+  },
+  {
+    "revision": "351e9eb361b2038313d3",
+    "url": "/reactToDo/static/js/runtime~main.351e9eb3.js"
+  },
+  {
+    "revision": "ff5eaed9fa3252f3c823620b2b44fa18",
+    "url": "/reactToDo/index.html"
+  }
+];
